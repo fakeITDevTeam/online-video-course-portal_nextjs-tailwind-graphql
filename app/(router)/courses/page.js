2b2385ef750ff36db308d5ed0062b10016173cde
fakeITDevTeam <dev5544455@gmail.com>
2024-02-13 14:30:35 +0700

    create side-navebar and header

---
+++ app/(router)/courses/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Courses() {
+  return (
+    <div className='grid grid-cols-3'>
+      {/* Left container */}
+      <div className='col-span-2'>
+        {/* Banner */}
+        Banner
+      </div>
+      {/* Right container */}
+      <div>
+        Right section
+      </div>
+    </div>
+  )
+}
+
+export default Courses
